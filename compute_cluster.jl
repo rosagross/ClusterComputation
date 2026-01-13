332afba571a0c5b5e50c14e08ebf1861462a2881
rosagross <grossmann.rc@gmail.com>
2026-01-13 16:43:24 +0100

    modify tutorial with my paths

---
+++ compute_cluster.jl
@@ -0,0 +1,18 @@
+# functionalities
+# - compute cluster (sum or volume)
+# - run permutation
+
+
+
+"""
+
+- `data`: shape is channel x time 
+- `threshold`:
+
+Calculate the sum of ann the values in the cluster, defined by the threshold. 
+
+"""
+
+function calc_cluster(data::)
+
+    # 
